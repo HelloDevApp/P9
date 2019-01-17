bebcf546516fe4c554bef489ca48432a919d029e
dylan <nais95100@icloud.com>
2019-01-17 13:53:25 +0100

    added the initial controller - and added the GradientView.swift file

---
+++ P9/View/GradientView.swift
@@ -0,0 +1,15 @@
+//
+//  GradientView.swift
+//  P9
+//
+//  Created by Mac Book Pro on 17/01/2019.
+//  Copyright © 2019 dylan. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    
+
+    
+}
