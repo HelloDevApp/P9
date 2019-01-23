1f0f080533f4f84990735f2ef0851a2499fb01d1
dylan <nais95100@icloud.com>
2019-01-23 23:02:25 +0100

    added the translateViewController file that will manage the translator scene

---
+++ P9/Controller/TranlateViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TranlateViewController.swift
+//  P9
+//
+//  Created by Mac Book Pro on 23/01/2019.
+//  Copyright © 2019 dylan. All rights reserved.
+//
+
+import UIKit
+
+class TranlateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
