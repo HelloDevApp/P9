0d0f619da89ee4605b9e5c5ee3ced50970a9188e
dylan <nais95100@icloud.com>
2019-01-23 22:31:21 +0100

    added a swift file containing a subclass of uiTextField to create custom textField

---
+++ P9/View/CustomTextField.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTextField.swift
+//  P9
+//
+//  Created by Mac Book Pro on 23/01/2019.
+//  Copyright © 2019 dylan. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    
+
+}
