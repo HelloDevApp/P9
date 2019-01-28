01a36cd8999144d2c6e0856df0f7dbda04ff9e21
dylan <nais95100@icloud.com>
2019-01-28 22:20:16 +0100

    added a new viewControler in the storyBoard for the weather part + added a controller in the code that manages it

---
+++ P9/Controller/WeatherViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherViewController.swift
+//  P9
+//
+//  Created by Mac Book Pro on 28/01/2019.
+//  Copyright © 2019 dylan. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
