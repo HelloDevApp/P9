f5829519b962d12f707d5d7ed0b4fd14a16d7613
dylan <nais95100@icloud.com>
2019-02-07 14:34:03 +0100

    added extensions to set up pickerView

--- P9/Controller/CurrencyViewController.swift
+++ P9/Controller/CurrencyViewController.swift
@@ -12,6 +12,29 @@ class CurrencyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // we fill the array with the different cases of enumeration
+        Data.shared.enumCaseToArray(language: nil, currency: true)
     }
 
+}
+
+// MARK: Picker View
+extension CurrencyViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    // number of pickerView components
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // number of rows in the component
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Data.shared.arrayCurrency.count
+    }
+    
+    // title of the selected row
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        // we change the color of the pickerView text and assign it a text
+        let attribute = NSAttributedString(string:Data.shared.arrayCurrency[row], attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        return attribute
+    }
 }
--- P9/Controller/TranlateViewController.swift
+++ P9/Controller/TranlateViewController.swift
@@ -12,7 +12,27 @@ class TranlateViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        Data.shared.enumCaseToArray(language: true, currency: nil)
     }
+}
 
+// MARK: Picker View
+extension TranlateViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    // number of pickerView components
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // number of rows in the component
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Data.shared.arrayLanguage.count
+    }
+    
+    // title of the selected row
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        // we change the color of the pickerView text and assign it a text
+        let attribute = NSAttributedString(string: Data.shared.arrayLanguage[row], attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        return attribute
+    }
 }
