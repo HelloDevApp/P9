a127dc875e02b512f3498a65c2232c2ec59c58ef
dylan <nais95100@icloud.com>
2019-03-22 14:06:11 +0100

    added a file that will create and configure the request for the translation part

---
+++ P9/Model/Api/TranslaterService.swift
@@ -0,0 +1,15 @@
+//
+//  TranslaterService.swift
+//  P9
+//
+//  Created by Mac Book Pro on 22/03/2019.
+//  Copyright © 2019 dylan. All rights reserved.
+//
+
+import Foundation
+
+class TranslaterService {
+    
+    static var shared = TranslaterService()
+    
+}
