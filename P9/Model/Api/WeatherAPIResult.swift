a3e5c23561cea61ba87837a37ab0744dc26e58a5
dylan <nais95100@icloud.com>
2019-03-29 15:53:50 +0100

    added a swift file that contains the decodable structures to receive the API responses

---
+++ P9/Model/Api/WeatherAPIResult.swift
@@ -0,0 +1,61 @@
+//
+//  WeatherAPIResult.swift
+//  P9
+//
+//  Created by Mac Book Pro on 29/03/2019.
+//  Copyright © 2019 dylan. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherAPIResult: Decodable {
+    let coord: Coord?
+    let weather: [Weather]?
+    let base: String?
+    let main: Main?
+    let visibility: Int?
+    let wind: Wind?
+    let clouds: Clouds?
+    let dt: Int?
+    let sys: Sys?
+    let id: Int
+    let name: String
+    let cod: Int
+}
+
+struct Coord: Decodable {
+    let lon: Double?
+    let lat: Double?
+}
+
+struct Weather: Decodable {
+    let id: Int?
+    let main: String?
+    let description: String?
+    let icon: String?
+}
+
+struct Main: Decodable {
+    let temp: Double?
+    let pressure: Int?
+    let humidity: Int?
+    let temp_min: Double?
+    let temp_max: Double?
+}
+
+struct Wind: Decodable {
+    let speed: Double?
+    let deg: Int?
+}
+struct Clouds: Decodable {
+    let all: Int?
+}
+
+struct Sys: Decodable {
+    let type: Int?
+    let id: Int?
+    let message: Double?
+    let country: String?
+    let sunrise: Int?
+    let sunset: Int?
+}
