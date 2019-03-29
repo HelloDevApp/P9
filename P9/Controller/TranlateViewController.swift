fa5e665a68b8b338c77413343433640d8b36b14c
dylan <nais95100@icloud.com>
2019-03-29 13:23:24 +0100

    added setup() method which prepares variables / gestures etc... + added method which allows to launch the request and display the result to the user + implementation of the button action

--- P9/Controller/TranlateViewController.swift
+++ P9/Controller/TranlateViewController.swift
@@ -18,24 +18,54 @@ class TranlateViewController: UIViewController {
     
     @IBOutlet weak var resultTextView: CustomTextView!
     
+    // contains the value of the case located in the middle of the language array.
+    var languagesCountDivideByTwo = Languages.allCases[Languages.allCases.count/2].rawValue
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        languageDestinationLabel.text = Languages.allCases[Languages.allCases.count/2].rawValue
+        languageDestinationLabel.text = languagesCountDivideByTwo
     }
     
     @IBAction func buttonTranslateAction() {
-        
-        guard let textToTranslate =  textToTranslate.text else { return }
-        guard textToTranslate.isEmpty == false, textToTranslate != "" else { return }
-        guard textToTranslate != "Entrer du texte" else { return }
+//        getTargetLang()
+        guard let textToTranslate = textToTranslate.text else { print("no success1"); return }
+        guard textToTranslate.isEmpty == false, textToTranslate != "" else { print("no success2"); return }
+        guard textToTranslate != "Entrer du texte" else { print("no success3"); return }
         TranslaterService.shared.textToTranslate = textToTranslate
-        TranslaterService.shared.getTranslation { (success, translatedText) in
-            guard success else { print("pas de success"); return }
-            guard let translatedText = translatedText else { print("pas de reponse"); return }
+        TranslaterService.shared.replaceMultipleCharactersForRequest()
+        launchRequest()
+    }
+    
+    func setup() {
+        view.addGestureToHideKeyboard()
+        SpecialCharactersSign.createDictionnaryOfSpecialCharactersSignAndCodes()
+        TranslaterService.shared.targetLang = "\(Languages.allCases[Languages.allCases.count/2])"
+        print(TranslaterService.shared.targetLang)
+    }
+    
+    func launchRequest() {
+        
+        TranslaterService.shared.getTranslation { (success, translations) in
+            
+            guard success else { return }
+            guard let translations = translations else { return }
+            guard let translatedText = translations.translatedText else { return }
+            let translatedTextFinal = TranslaterService.shared.replaceCharactersOfTranslatedText(translatedText: translatedText)
+            
             DispatchQueue.main.async {
-            self.resultTextView.text = translatedText
+                self.resultTextView.text = translatedTextFinal
+                print(translatedTextFinal)
             }
         }
     }
 }
+
+// a voir avec mentor si preferable cette methode ou garder le changement de valeur dans la methode setup et lorsque le pickerView change de selection
+//    func getTargetLang() {
+//        for nameCase in Languages.allCases {
+//            if nameCase.rawValue == languageDestinationLabel.text {
+//                print("\(nameCase)")
+//            }
+//        }
+//    }
