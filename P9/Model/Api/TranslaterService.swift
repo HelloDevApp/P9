f79263c36153604808cbe155c3d8b7646462ade5
dylan <nais95100@icloud.com>
2019-03-29 13:14:32 +0100

    added code that creates / configures the request and manages the sending

--- P9/Model/Api/TranslaterService.swift
+++ P9/Model/Api/TranslaterService.swift
@@ -12,4 +12,30 @@ class TranslaterService {
     
     static var shared = TranslaterService()
     
+    var textToTranslate = ""
+    var targetLang = ""
+    let url = URL(string: "https://translation.googleapis.com/language/translate/v2")!
+    
+    func getTranslation(callback: @escaping (Bool, Translation?) -> Void) {
+        
+        let parameters = "?key=\(APIKey.shared.apiKeyTranslater)&q=\(textToTranslate)&target=\(targetLang)&source=fr"
+        print(parameters)
+        guard let urlComplete = URL(string: "\(url)\(parameters)") else { print("erreur ulrComplete"); return }
+        print(urlComplete)
+        let session = URLSession(configuration: .default)
+        
+        let request = URLRequest(url: urlComplete)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else { print("erreur data nil || error "); callback(false, nil); return }
+            guard let json = try? JSONDecoder().decode(TranslaterAPIResult.self, from: data) else { print("erreur json decoder"); callback(false,nil); return }
+            guard let json_Data = json.data else { print("erreur3"); callback(false, nil); return }
+            guard let json_Data_Translations = json_Data.translations else { print("erreur json data translations nil"); callback(false, nil); return }
+            guard let translation_Last = json_Data_Translations.last else { print("erreur  translations last nil"); callback(false, nil); return }
+            guard let translation = translation_Last.translatedText else { print("tranlation nil"); callback(false, nil); return }
+            let translations = Translation(translatedText: translation)
+            callback(true, translations)
+        }
+        task.resume()
+    }
 }
