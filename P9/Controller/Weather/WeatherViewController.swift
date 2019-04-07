04a7c0e813c06a26b8541a2b91442ebd4b176b48
dylan <nais95100@icloud.com>
2019-04-07 23:54:49 +0200

    addition of user interface outlets (weather part)

--- P9/Controller/Weather/WeatherViewController.swift
+++ P9/Controller/Weather/WeatherViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var cityLeft: CustomLabel!
+    @IBOutlet weak var dateLeft: UILabel!
+    @IBOutlet weak var iconLeft: UIImageView!
+    @IBOutlet weak var currentTempLeft: UILabel!
+    @IBOutlet weak var descriptionLeft: CustomLabel!
+    @IBOutlet weak var humidityLeft: CustomLabel!
+    @IBOutlet weak var cityRight: CustomLabel!
+    @IBOutlet weak var dateRight: UILabel!
+    @IBOutlet weak var iconRight: UIImageView!
+    @IBOutlet weak var currentTempRight: UILabel!
+    @IBOutlet weak var descriptionRight: CustomLabel!
+    @IBOutlet weak var humdidityRight: CustomLabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
