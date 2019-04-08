6ea2688d342cb0791b3597aa6f1402c2c633febf
dylan <nais95100@icloud.com>
2019-04-08 14:42:03 +0200

    added the Weather.swift file

---
+++ P9/Model/Weather/Weather.swift
@@ -0,0 +1,22 @@
+//
+//  Weather.swift
+//  P9
+//
+//  Created by Mac Book Pro on 30/03/2019.
+//  Copyright © 2019 dylan. All rights reserved.
+//
+
+import Foundation
+
+
+class Weather_ {
+    
+    static let shared = Weather_()
+    
+    private let _country = ["New york,us", "Paris,fr"]
+    
+    var country: [String] {
+        return _country
+    }
+    
+}
